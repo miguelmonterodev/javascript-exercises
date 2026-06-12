9fd336b9ae2167fe08370dc6e4f4d160cfa72e51
Miguel Montero <192806710+miguelmonterodev@users.noreply.github.com>
2026-06-12 12:02:21 +0100

    Temperature conversion
    Create two functions to convert Celsius to Fahrenheit and Fahrenheit to Celsius

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let celsius = (tempF - 32) * 5 / 9
+  return parseInt(celsius.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let fahrenheit = (tempC * 9 / 5) + 32
+  return parseInt(fahrenheit.toFixed(1))
 };
 
 // Do not edit below this line

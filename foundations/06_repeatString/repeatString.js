7d44609715a6c988eae444c5a0b6ffc935e6efb6
Miguel Montero <192806710+miguelmonterodev@users.noreply.github.com>
2026-06-12 10:59:48 +0100

    Repeat String
    Create a function with two parameters/arguments string and num
    Repeate the string as many times as num

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,4 +1,9 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let str = ""
+    for(let i = 0; i < num; i++) {
+        str += string
+    }
+    return str
 
 };
 
